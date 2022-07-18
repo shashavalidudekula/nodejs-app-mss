f50d9de9b6c29ba7db57c70d69399c519a0e5c1d
Shashavali Dudekula <6346119+shashavalidudekula@users.noreply.github.com>
2022-07-18 10:36:22 +0530

    Update app.js

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ console.log(__dirname+"/images/mithunlogo.jpg");
 */
 app.get('/shasha', function(request, response) {
     //response.send("<h2><center>Welcome to Node JS app</h2>");
+    res.set("Content-Type","text/html");
     response.write("<h2><center><u>Node JS  Application </u></center></h2>");
 	
     response.write("<h2><center>Welcome to  Mithun Technologies. Please Contact +91-9980923226 for more information or send an email to devopstrainingblr@gmail.com <center></h2>" );
